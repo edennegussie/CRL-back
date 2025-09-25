ef67a55972aec6fc4fba48282036fdc55a3d5087
Eden Negussie <educasmail@gmail.com>
2025-09-25 11:45:14 +0300

    removed unused endpoint

--- index.js
+++ index.js
@@ -68,13 +68,15 @@ app.get('/resources', async (req, res) => {
   }
 });
 
-  // Catch-all for undefined routes (404)
-  app.use((req, res, next) => {
-    res.status(404).send(`Route "${req.originalUrl}" not found. Please enter the correct endpoint.`);
-  });
+
+// Catch-all for undefined routes (404)
+app.use((req, res, next) => {
+   res.status(404).send(`Route "${req.originalUrl}" not found. Please enter the correct endpoint.`);
+});
 
 // Start server
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Resources API: http://localhost:${PORT}/resources`);
 });
+
